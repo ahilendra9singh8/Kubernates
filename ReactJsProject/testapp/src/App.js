9305edebfffad94dee0a4a2b1af842aa69dce096
Shailu08 <ssinghlodhi08@gmail.com>
2025-04-13 12:09:12 +0530

    add project

--- ReactJsProject/testapp/src/App.js
+++ ReactJsProject/testapp/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-         This demo kubernates project!
+         This demo kubernates project version v02!
         </p>
         <a
           className="App-link"
